ad677e232370cad379e64c0ad490479296ee26a6
maksonech <maksim-chupikov@yandex.ru>
2020-12-01 19:25:32 +0700

    ctrl shft l

--- 1.py
+++ 1.py
@@ -2,12 +2,15 @@ import csv
 
 a = 'L-карнитин табл шип №20 Консумед;Малкут;Новосибирск, ул. Б.Богаткова, д.264;420.00'
 
+
 def write_to_file(data):
     print('write to file.....')
     with open('test' + '.csv', 'w', newline='') as file:
         a_pen = csv.writer(file)
-        #for d in data.split():
-            #print(data)
+        # for d in data.split():
+        # print(data)
         a_pen.writerow([data])
-        #ppfpp
+        # ppfpp
+
+
 write_to_file(a)
